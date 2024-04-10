a7de1e801bfe2ff9ff1446af70c2d42c21bd2ec5
C. Tinkelrot <chavit@skyvar.co.il>
2024-04-10 11:20:15 +0300

    try

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,9 +1,26 @@
 <?php
+use App\Models\User;
 
 
 
-it('returns a successful response', function () {
+// it('returns a successful response', function () {
+//     $response = $this->get('/');
+
+//     $response->assertStatus(200);
+// });
+
+it('redirects unauthorized users to the login page', function () {
+    $response = $this->get('/');
+
+    $response->assertStatus(302);
+    $response->assertRedirect('/login');
+});
+
+it('allows logged in users to access the main panel', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
     $response = $this->get('/');
     
-    $response->assertStatus(200);
+    $response->assertStatus(200); 
 });
