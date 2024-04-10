aa71e606445b56a1ff0414c260ae83ace2d392bb
C. Tinkelrot <chavit@skyvar.co.il>
2024-04-10 13:35:52 +0300

    try

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,15 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    public function asUser($user = null): static 
+    {
+        $this->actingAs($user ?? User::factory()->create());
+
+        return $this;
+    }
 }
