35379cdb1750e40c6243db0ac01397d8c8039242
C. Tinkelrot <chavit@skyvar.co.il>
2024-04-11 09:38:08 +0300

    try

--- tests/Feature/RoutesTest.php
+++ tests/Feature/RoutesTest.php
@@ -1,6 +1,4 @@
 <?php
-// use App\Models\User;
-
 
 it('redirects unauthorized users to the login page', function () {
     $response = $this
@@ -9,14 +7,9 @@ it('redirects unauthorized users to the login page', function () {
 });
 
 it('allows logged in users to access the main panel', function () {
-    // $user = User::factory()->create();
-    // $this->actingAs($user);
     $this
         ->asUser()
         ->get('/')
         ->assertStatus(200);
 
-    // $response = $this->get('/');
-
-    // $response->assertStatus(200); 
 });
